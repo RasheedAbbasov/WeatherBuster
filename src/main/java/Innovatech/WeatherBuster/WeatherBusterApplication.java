080ab4c1cb024d47bc760ca70e9d797773360d25
CosmoUrMom <rasheedabbasov@gmail.com>
2025-11-25 14:30:06 -0500

    init commit

---
+++ src/main/java/Innovatech/WeatherBuster/WeatherBusterApplication.java
@@ -0,0 +1,13 @@
+package Innovatech.WeatherBuster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherBusterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherBusterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Innovatech/WeatherBuster/WeatherBusterApplicationTests.java
@@ -0,0 +1,13 @@
+package Innovatech.WeatherBuster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherBusterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
