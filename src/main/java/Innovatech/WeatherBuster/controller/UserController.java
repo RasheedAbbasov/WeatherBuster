a77d8b117cd58ba3b0ff7ef77ea677a4ab6e713d
CosmoUrMom <rasheedabbasov@gmail.com>
2025-12-30 18:48:07 -0500

    Added controller and service

---
+++ src/main/java/Innovatech/WeatherBuster/controller/UserController.java
@@ -0,0 +1,17 @@
+package Innovatech.WeatherBuster.controller;
+
+import Innovatech.WeatherBuster.model.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import Innovatech.WeatherBuster.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+   private UserService userService;
+
+   
+
+    
+}
