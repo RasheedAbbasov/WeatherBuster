5075279f1206b70c989d04611b7cb364ef0c6d82
CosmoUrMom <rasheedabbasov@gmail.com>
2026-01-07 02:05:14 -0500

    Adde basic controllers to UserController

--- src/main/java/Innovatech/WeatherBuster/controller/UserController.java
+++ src/main/java/Innovatech/WeatherBuster/controller/UserController.java
@@ -1,6 +1,11 @@
 package Innovatech.WeatherBuster.controller;
 
 import Innovatech.WeatherBuster.model.User;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import Innovatech.WeatherBuster.service.UserService;
@@ -11,7 +16,20 @@ public class UserController {
 
    private UserService userService;
 
-   
+   public UserController (UserService userService) {
+      this.userService = userService;
+   }
 
-    
+   @GetMapping
+   public List<User> getAllUsers() {
+      return userService.getAllUsers();
+   }
+
+   @GetMapping("{id}")
+   public User getUserByid(@PathVariable Integer id) {
+      return userService.getUserById(id);
+   }
+
+   
+   
 }
